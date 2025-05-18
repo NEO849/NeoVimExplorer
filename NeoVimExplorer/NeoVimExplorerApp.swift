b174d7f793c667d07b5d6498216ad7a83639db0b
neo849 <f.michi84.989@gmail.com>
2025-05-18 16:40:02 +0200

    Initial Commit

---
+++ NeoVimExplorer/NeoVimExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoVimExplorerApp.swift
+//  NeoVimExplorer
+//
+//  Created by Michael Fleps on 18.05.25.
+//
+
+import SwiftUI
+
+@main
+struct NeoVimExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
